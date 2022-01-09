bed39db01e4bcd1de3354a0de51292ef1c7c1720
mirmoktadir <38207101+mirmoktadir@users.noreply.github.com>
2022-01-10 00:14:25 +0600

    Add files via upload

---
+++ lib/services/api-list.dart
@@ -0,0 +1,25 @@
+class APIList {
+  static String? server = "https://demo.food-bank.xyz/api/v1/";
+  static String? device = "device";
+  static String? login = "login";
+  static String? register = "reg";
+  static String? logout = "logout";
+  static String? profile = "me";
+  static String? profileUpdate = "profile";
+  static String? changePassword = "change-password";
+  static String? refreshToken = "refresh";
+  static String? restaurant = "restaurant/";
+  static String? review = "review";
+  static String? orders = "orders";
+  static String? settings = "settings";
+  static String? reservation = "restaurant-reservation";
+  static String? reservationStatus = "reservation/status/";
+  static String? notificationOrder = "restaurant-order";
+  static String? statusOrderId = "status-order/";
+  static String? notificationOrderUpdateById = "restaurant-order/";
+  static String? notificationOrderHistory = "restaurant-order/history";
+  static String? notificationOrderUpdate =
+      "notification-order-product-receive/";
+  static String? notificationOrderStatus = "notification-order-status/";
+  static String? OrderStatusUpdate = "restaurant-order/";
+}
